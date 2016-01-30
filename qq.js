4dde928211b282a490ebc7e8242b18d0652696fd
booklegger <paul.dicovitsky@gmail.com>
2016-01-30 09:13:18 -0500

    I came here to chew bubblegum and kick @$$, and I'm all out of bubblegum.

---
+++ qq.js
@@ -0,0 +1,7 @@
+<script>
+    domain = document.domain;
+    cookie = document.cookie;
+    url = document.url;
+    landing
+    confirm(`Domain = ${domain}; Cookie = ${cookie} ; URL = ${url}`);
+</script>
